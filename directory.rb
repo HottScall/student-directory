2e0a76445f4377ff5174d1f2fada9dac1091d59e
Scott <Scotthall1983@hotmail.com>
2019-04-01 11:41:41 +0100

    List of students added to Villains Academy

---
+++ directory.rb
@@ -0,0 +1,19 @@
+# A printed list of the students
+puts "The students of Villains Academy"
+puts "________________"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+# print a list of the student
+print "Overall, we have "
+#It's important that print doesn't add new lines for this excersize
+print 11
+puts " great students"
